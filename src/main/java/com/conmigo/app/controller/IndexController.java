24d29188f51648fa8a027c200181122c5747253d
Daniel S. Blanco Cuadrado <danielblancocuadrado@gmail.com>
2017-02-14 15:46:38 +0100

    dsbc: quitamos constante

--- src/main/java/com/conmigo/app/controller/IndexController.java
+++ src/main/java/com/conmigo/app/controller/IndexController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	private final static int HIGHLIGHT_ELEMENTS = 3;
-
 	@GetMapping( value = "/" )
 	public String home( final Model model ) {
 		return "site.index";
