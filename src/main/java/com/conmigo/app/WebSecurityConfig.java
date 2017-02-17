aadc63c6bf3c85f7db033892432683c247bf2cf5
Daniel S. Blanco Cuadrado <danielblancocuadrado@gmail.com>
2017-02-17 13:53:54 +0100

    dsbc: quitamos configuracion no necesaria

--- src/main/java/com/conmigo/app/WebSecurityConfig.java
+++ src/main/java/com/conmigo/app/WebSecurityConfig.java
@@ -1,19 +1,12 @@
 package com.conmigo.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.social.connect.ConnectionFactoryLocator;
-import org.springframework.social.connect.UsersConnectionRepository;
-import org.springframework.social.connect.web.ProviderSignInUtils;
 import org.springframework.social.security.SpringSocialConfigurer;
 
 @Configuration
@@ -55,28 +48,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				// Adds the SocialAuthenticationFilter to Spring Security's filter chain. Nos permite que funcione la url /auth/provider
 				.and().apply( new SpringSocialConfigurer().postLoginUrl( "/" ).alwaysUsePostLoginUrl( true ) );
 		// @formatter:on
-	}
-
-	@Bean( name = "passwordEncoder" )
-	public PasswordEncoder passwordencoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Autowired
-	public void configAuthentication( final AuthenticationManagerBuilder auth ) throws Exception {
-		auth.userDetailsService( userDetailsService ).passwordEncoder( passwordencoder() );
-	}
-
-	/**
-	 * Crea el metodo que permite obtener los connection sociales de la request
-	 *
-	 * @param connectionFactoryLocator
-	 * @param connectionRepository
-	 * @return
-	 */
-	@Bean
-	public ProviderSignInUtils providerSignInUtils( final ConnectionFactoryLocator connectionFactoryLocator, final UsersConnectionRepository connectionRepository ) {
-		return new ProviderSignInUtils( connectionFactoryLocator, connectionRepository );
 	}
 
 }
