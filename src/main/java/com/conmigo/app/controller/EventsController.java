d1ef974c8a05b936dd59de6f7911a4e1afbe9b54
Daniel S. Blanco Cuadrado <danielblancocuadrado@gmail.com>
2017-02-19 21:08:49 +0100

    dsbc: obtenemos los eventos

--- src/main/java/com/conmigo/app/controller/EventsController.java
+++ src/main/java/com/conmigo/app/controller/EventsController.java
@@ -1,14 +1,23 @@
 package com.conmigo.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.conmigo.app.service.EventService;
+
 @Controller
 public class EventsController {
 
+	private final static String PAGE = "site.events";
+
+	@Autowired
+	EventService eService;
+
 	@GetMapping( value = "/events" )
 	public String home( final Model model ) {
-		return "site.events";
+		model.addAttribute( "events", eService.findAll() );
+		return PAGE;
 	}
 }
--- src/main/webapp/WEB-INF/pages/events.jsp
+++ src/main/webapp/WEB-INF/pages/events.jsp
@@ -3,4 +3,10 @@
   <h1>Events Page</h1>
   <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
   <p>Poner barra buscador</p>
+  
+  <ul>
+  <c:forEach items="${events}" var="item" varStatus="status">
+  	<li>${item.name }</li>
+  </c:forEach>
+  </ul>
 </div>
