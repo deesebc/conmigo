67479043a372b27be40951b36a621c981577ae62
Daniel S. Blanco Cuadrado <danielblancocuadrado@gmail.com>
2017-02-19 12:46:55 +0100

    DSBC: QUitamos metodo duplicado

--- src/main/java/com/conmigo/app/controller/LoginController.java
+++ src/main/java/com/conmigo/app/controller/LoginController.java
@@ -1,13 +1,7 @@
 package com.conmigo.app.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +15,7 @@ public class LoginController {
 	private final static Logger LOGGER = LoggerFactory.getLogger( LoginController.class );
 
 	private final static String REDIRECT_INDEX = "redirect:/";
+	private final static String PAGE = "site.login";
 
 	public LoginController() {
 		super();
@@ -28,21 +23,12 @@ public class LoginController {
 
 	@RequestMapping( value = "/login", method = RequestMethod.GET )
 	public String login( @RequestParam( value = "error", required = false ) final String error ) {
-		String retorno = "site.login";
+		String retorno = PAGE;
 		if( SecurityUtil.isFullyAuthenticated() ) {
 			retorno = REDIRECT_INDEX;
 		}
 
 		return retorno;
-	}
-
-	@RequestMapping( value = "/logout", method = RequestMethod.GET )
-	public String logout( final HttpServletRequest request, final HttpServletResponse response ) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if( auth != null ) {
-			new SecurityContextLogoutHandler().logout( request, response, auth );
-		}
-		return REDIRECT_INDEX;
 	}
 
 }
