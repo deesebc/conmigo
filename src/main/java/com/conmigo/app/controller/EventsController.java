c5fbc79bfebda15cc70b735d45747dab72d66a4e
Daniel S. Blanco Cuadrado <danielblancocuadrado@gmail.com>
2017-02-21 17:20:11 +0100

    dsbc: mejoramos la obtención de la búsqueda

--- src/main/java/com/conmigo/app/controller/EventsController.java
+++ src/main/java/com/conmigo/app/controller/EventsController.java
@@ -1,7 +1,5 @@
 package com.conmigo.app.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.conmigo.app.dto.EventDto;
 import com.conmigo.app.service.EventService;
@@ -30,8 +29,7 @@ public class EventsController {
 	}
 
 	@PostMapping( value = "/search" )
-	public String search( final HttpServletRequest request, final Model model ) {
-		String name = request.getParameter( "name" );
+	public String search( @RequestParam( "name" ) final String name, final Model model ) {
 		PageRequest pageRequest = new PageRequest( 0, 10 );
 		Page<EventDto> page = eService.findByNameContainingIgnoreCase( pageRequest, name );
 		model.addAttribute( "events", page.getContent() );
